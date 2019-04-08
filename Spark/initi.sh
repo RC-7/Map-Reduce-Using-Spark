156e7ad97578c6588e1b2c0b5fb43b17b298dcd2
RC-7 <rashcassim@gmail.com>
2019-04-08 19:48:26 +0200

    added much better output formattinggit add .

---
+++ Spark/initi.sh
@@ -0,0 +1,2 @@
+export PYSPARK_DRIVER_PYTHON=/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+export PYSPARK_PYTHON=/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
--- Spark/main.py
+++ Spark/main.py
@@ -3,7 +3,6 @@ class lineNum:
 		self.number=0;
 
 	def access(self):
-		print(self.number)
 		return self.number
 
 	def incr(self):
@@ -22,13 +21,11 @@ def linesToWordsFunc(line):
 
 
 
-# /Library/Frameworks/Python.framework/Versions/3.7/bin/python3
-
 def hailMary(word):
 	if word.isdigit():
 		ln.incr()
 
-	return (word,ln.access())
+	return (word,[ln.access()])
 
 
 
@@ -38,24 +35,13 @@ lowerCase = inFile.map(lambda x: x.lower())
 
 split=lowerCase.flatMap(lambda line: line.split(" "))
 
-mappedPairs = split.map(hailMary)					# instead of putting a one put line
+mappedPairs = split.map(hailMary)
 
 
-split2=mappedPairs.flatMap(lambda line: line.split(" "))
-mappedPairs2 = split.map(lambda word: (word, 1))	
-print(type(split))
-
-
-# for x in mappedPairs.collect():
-# 	# i=i+1
-# 	# # p=x.split(" ")
-# 	# p=x.map(lambda line: line.split(" "))
-# 	# mappedPairs = p.map(lambda word: (word, i))
-# 	print(x)
-
-reduceList = mappedPairs.reduceByKey(lambda a, b: [a,b])
-groupedList = mappedPairs.groupByKey()
+reduceList = mappedPairs.reduceByKey(lambda a, b: a+b)
 
 
 for x in reduceList.collect():
+
+
     print (x)
